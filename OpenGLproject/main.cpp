b6c6782a97e995a6646bdb7754051abaa91f439b
aneeb rasool. <aneeb.rasool@outlook.com>
2026-06-30 18:36:39 -0400

    triangle?? (idk what this does lol)

--- OpenGLproject/main.cpp
+++ OpenGLproject/main.cpp
@@ -3,6 +3,22 @@
 #include<GLFW/glfw3.h>
 
 
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main() {
 
 	// initialize the window
@@ -13,17 +29,25 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLfloat vertices[] = //source code for fragment shader
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
+
+
 
 	// (parameters of making a window (in order):
 	//width, height, name, full screen?, idk 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "openglwindow", NULL, NULL);
 
-	if(window == NULL) // error check incase window fails to create
-		{
+	if (window == NULL) // error check incase window fails to create
+	{
 		std::cout << "Failed to create window" << std::endl;
 		glfwTerminate();
 		return -1;
-		}
+	}
 
 	//contexts tells which functions (the following) should apply to what window
 	glfwMakeContextCurrent(window);
@@ -33,20 +57,69 @@ int main() {
 
 	glViewport(0, 0, 800, 800); //area of window opengl renders!
 
+	//unsigned aka pos integer
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Attach Fragment Shader source to the Fragment Shader Object
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	// Compile the Vertex Shader into machine code
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(vertexShader);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f); //1st 3 nums colour, last transparency
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
 
-//since it terminates instantly, while loop keeps it runningtill user ends it themselves!
-	while (!glfwWindowShouldClose(window)) 
+	//since it terminates instantly, while loop keeps it runningtill user ends it themselves!
+	while (!glfwWindowShouldClose(window))
 	{
+
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f); //1st 3 nums colour, last transparency
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+
 		//allows window to handle things like resise, and input possiblu
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+
 	glfwDestroyWindow(window); //destroys the current window u got
-	
+
 	glfwTerminate(); //termintates everything glfw related
 
 	return 0;
